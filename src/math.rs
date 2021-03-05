d08dff53d85d5ad4d5a18955e963dc20f258e4fe
pr0n1x <pr0n1x@yandex.ru>
2021-03-06 02:09:03 +0700

    Implemented Point and Rectangle math primitives

--- src/math.rs
+++ src/math.rs
@@ -1,87 +1,191 @@
 #![allow(unused_variables)]
-#[derive(Default, Clone, Debug)]
-pub struct Point<T> {
+
+use num_traits::{FromPrimitive, Float};
+
+
+#[derive(Default, Clone, Copy, Debug)]
+pub struct Point<T>
+    where T: Float + FromPrimitive
+{
     pub x: T,
     pub y: T,
 }
 
-impl<T> Point<T> {
+impl<T> Point<T>
+    where T: Float + FromPrimitive
+{
+
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+
     /// Get the straight line (Euclidean) distance between the origin (0, 0) and this point
     pub fn magnitude(&self) -> T {
-        unimplemented!()
+        let zero = FromPrimitive::from_f64(0.0).expect("Can't cast from int to Float");
+        self.distance_to(Self{x: zero, y: zero})
     }
 
     /// Returns the distance between this and other
-    pub fn distance_to(&self, other: Point<T>) -> T {
-        unimplemented!()
+    pub fn distance_to(&self, target: Point<T>) -> T {
+        self.squared_distance_to(target).sqrt()
     }
 
     /// Returns the squared distance between this and other
-    pub fn squared_distance_to(&self, other: Point<T>) -> T {
-        unimplemented!()
+    pub fn squared_distance_to(&self, target: Point<T>) -> T {
+        (target.x - self.x).powi(2) + (target.y - self.y).powi(2)
     }
 
-    // TODO: +,-,*, ==
+    // TODO: +,-,*
 }
 
-#[derive(Default, Clone, Debug)]
-pub struct Rectangle<T> {
-    pub left: T,
-    pub top: T,
-    pub width: T,
-    pub height: T,
+impl<T> PartialEq for Point<T>
+    where T: Float + FromPrimitive
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
 }
 
-impl<T> Rectangle<T> {
-    pub fn get_bottom(&self) -> T {
-        unimplemented!()
+#[derive(Default, Clone, Copy, Debug)]
+pub struct Rectangle<T>
+    where T: Float + FromPrimitive
+{
+    // TODO: Replace "top" and "left" to "origin: Point<T>" it will give us ability to use any of rectangle points as origin point
+    left: T,
+    top: T,
+    width: T,
+    height: T,
+}
+
+impl<T: Float + FromPrimitive> Rectangle<T>
+    where T: Float + FromPrimitive
+{
+    pub fn new(left: T, top: T, width: T, height: T) -> Self {
+        Self { left, top, width, height }
     }
 
-    pub fn get_bottom_left(&self) -> Point<T> {
-        unimplemented!()
+    #[inline]
+    pub fn get_left(&self) -> T {
+        self.left
     }
 
-    pub fn get_bottom_right(&self) -> Point<T> {
-        unimplemented!()
+    #[inline]
+    pub fn get_top(&self) -> T {
+        self.top
+    }
+
+    #[inline]
+    pub fn get_width(&self) -> T {
+        self.width
+    }
+
+    #[inline]
+    pub fn get_height(&self) -> T {
+        self.height
     }
 
     pub fn get_right(&self) -> T {
-        unimplemented!()
+        self.left + self.width
+    }
+
+    pub fn get_bottom(&self) -> T {
+        self.top + self.height
+    }
+
+    pub fn get_bottom_left(&self) -> Point<T> {
+        Point {
+            x: self.left,
+            y: self.get_bottom()
+        }
+    }
+
+    pub fn get_bottom_right(&self) -> Point<T> {
+        Point {
+            x: self.get_right(),
+            y: self.get_bottom()
+        }
     }
 
     pub fn get_top_left(&self) -> Point<T> {
-        unimplemented!()
+        Point {
+            x: self.left,
+            y: self.top
+        }
     }
 
     pub fn get_top_right(&self) -> Point<T> {
-        unimplemented!()
+        Point {
+            x: self.get_right(),
+            y: self.top
+        }
     }
 
-    /// Returns a new rectangle which completely contains this and other
-    pub fn bounding_box(&self, other: Rectangle<T>) -> Rectangle<T> {
-        unimplemented!()
+    /// Returns Bounds tuple (top, right, bottom, left)
+    #[inline]
+    pub fn bounds_tuple(&self) -> (T, T, T, T) {
+        (
+            self.top,
+            self.get_right(),
+            self.get_bottom(),
+            self.left
+        )
     }
 
-    /// Tests whether another is inside or along the edges of this
-    pub fn contains_point(&self, another: Point<T>) -> bool {
-        unimplemented!()
+    /// Returns a new rectangle which completely contains self and other
+    pub fn bounding_box(&self, other: Self) -> Self {
+        let (s_top, s_right, s_bottom, s_left) = self.bounds_tuple();
+        let (o_top, o_right, o_bottom, o_left) = other.bounds_tuple();
+        let (top, left, right, bottom) = (
+            if s_top > o_top { s_top } else { o_top },
+            if s_left > o_left { s_left } else { o_left },
+            if s_right > o_right { s_right } else { o_right },
+            if s_bottom > o_bottom { s_bottom } else { o_bottom }
+        );
+        Self {
+            top, left,
+            width: right - left,
+            height: bottom - top
+        }
+    }
+
+    /// Tests whether point is inside or along the edges of this
+    pub fn contains_point(&self, point: Point<T>) -> bool {
+        let (top, right, bottom, left) = self.bounds_tuple();
+        point.x >= left && point.x <= right && point.y >= top && point.y <= bottom
     }
 
     /// Tests whether this entirely contains another
-    pub fn contains_rectangle(&self, another: Rectangle<T>) -> bool {
-        unimplemented!()
+    pub fn contains_rectangle(&self, other: Self) -> bool {
+        let (s_top, s_right, s_bottom, s_left) = self.bounds_tuple();
+        let (o_top, o_right, o_bottom, o_left) = other.bounds_tuple();
+        s_left <= o_left && s_right >= o_right && s_top <= o_top && s_bottom >= o_bottom
     }
 
     /// Returns true if this intersects other
-    pub fn intersects(&self, other: Rectangle<T>) -> bool {
-        unimplemented!()
+    pub fn intersects(&self, other: Self) -> bool {
+        self.contains_point(other.get_top_left())
+            || self.contains_point(other.get_top_right())
+            || self.contains_point(other.get_bottom_right())
+            || self.contains_point(other.get_bottom_left())
     }
-
-    // TODO: ==
 }
 
-#[derive(Default, Clone, Debug)]
-pub struct Matrix<T> {
+impl<T> PartialEq for Rectangle<T>
+    where T: Float + FromPrimitive
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.top == other.top
+            && self.left == other.left
+            && self.width == other.width
+            && self.height == other.height
+    }
+}
+
+
+#[derive(Default, Clone, Copy, Debug)]
+pub struct Matrix<T>
+    where T: Float + FromPrimitive
+{
     pub a: T,
     pub b: T,
     pub c: T,
@@ -90,7 +194,11 @@ pub struct Matrix<T> {
     pub f: T,
 }
 
-impl<T> Matrix<T> {
+/// Interface of SVGMatrix:
+/// @see https://developer.mozilla.org/en-US/docs/Web/API/SVGMatrix
+impl<T> Matrix<T>
+    where T: Float + FromPrimitive
+{
     pub fn flip_x(&self) -> Matrix<T> {
         unimplemented!()
     }
@@ -126,4 +234,57 @@ impl<T> Matrix<T> {
         unimplemented!()
     }
     // TODO: ==
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Point;
+    use super::Rectangle;
+
+    #[test]
+    fn test_point_distance() {
+        assert_eq!(Point::new(0.0, 10.0).distance_to(Point::new(100.0, 10.0)), 100.0);
+        assert_eq!(Point::new(0.0, 0.0).squared_distance_to(Point::new(100.0, 100.0)), 20_000_f64);
+        assert_eq!(Point::new(0.0, 0.0).distance_to(Point::new(100.0, 100.0)), 20_000_f64.sqrt());
+        assert_eq!(Point::new(100.0, 100.0).magnitude(), 20_000_f64.sqrt());
+    }
+
+    #[test]
+    fn test_rect_points() {
+        let rect = Rectangle::new(100.0, 100.0, 100.0, 100.0);
+        assert_eq!(rect.get_top_left(), Point::new(100.0, 100.0));
+        assert_eq!(rect.get_top_right(), Point::new(200.0, 100.0));
+        assert_eq!(rect.get_bottom_right(), Point::new(200.0, 200.0));
+        assert_eq!(rect.get_bottom_left(), Point::new(100.0, 200.0));
+        assert_ne!(rect.get_bottom_left(), Point::new(100.0, 201.0));
+        //assert_eq!(rect.get_top_right(), Point::new(100.0, 100.0));
+    }
+
+    #[test]
+    fn test_rect_contains_point() {
+        let rect = Rectangle::new(100.0, 100.0, 50.0, 50.0);
+        assert!(rect.contains_point(Point::new(120.0, 120.0)));
+        assert!(rect.contains_point(Point::new(100.0, 100.0)));
+        assert!(rect.contains_point(Point::new(120.0, 149.0)));
+        assert!(rect.contains_point(Point::new(150.0, 150.0)));
+        assert!(!rect.contains_point(Point::new(50.0, 50.0)));
+        assert!(!rect.contains_point(Point::new(120.0, 151.0)));
+        assert!(!rect.contains_point(Point::new(151.0, 120.0)));
+    }
+
+    #[test]
+    fn test_rect_intersection() {
+        let rect = Rectangle::new(100.0, 100.0, 100.0, 100.0);
+        assert!(rect.intersects(Rectangle::new(150.0, 150.0, 10.0, 10.0)));
+        assert!(rect.intersects(Rectangle::new(200.0, 200.0, 10.0, 10.0)));
+        assert!(!rect.intersects(Rectangle::new(201.0, 200.0, 10.0, 10.0)));
+
+        assert!(rect.intersects(Rectangle::new(0.0, 0.0, 100.0, 100.0)));
+        assert!(!rect.intersects(Rectangle::new(0.0, 0.0, 99.0, 100.0)));
+        assert!(!rect.intersects(Rectangle::new(0.0, 0.0, 100.0, 99.0)));
+
+        assert!( ! rect.intersects(Rectangle::new(0.0, 0.0, 100.0, 99.99999999999999)));
+        assert!(   rect.intersects(Rectangle::new(0.0, 0.0, 100.0, 99.999999999999999)));
+    }
 }
