4a4b915dbc30174c78542c95c0d7213d7b1ba408
Victor Dudochkin <dudochkin.victor@gmail.com>
2021-03-07 18:14:53 +0700

    color palletes

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -1,12 +1,137 @@
+use std::fmt;
+
+pub mod palette;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 pub enum Color {
     // These 3 are specified solely by their name.
     Red,
-    Blue,
     Green,
-    // These likewise tie `u32` tuples to different names: color models.
-    RGB(u32, u32, u32),
-    HSV(u32, u32, u32),
-    HSL(u32, u32, u32),
-    CMY(u32, u32, u32),
-    CMYK(u32, u32, u32, u32),
+    Blue,
+    /// A representation of the RGB (Red, Green, Blue) color format.
+    RGB(u8, u8, u8),
+    /// A representation of the RGBA (Red, Green, Blue, Alpha) color format.
+    RGBA(u8, u8, u8, u8),
+    /// A representation of the HSL (Hue, Saturation, Value) color format.
+    HSV(u16, u8, u8),
+    /// A representation of the HSL (Hue, Saturation, Lightness) color format.
+    HSL(u16, u8, u8),
+    /// A representation of the CMYK (Cyan, Magenta, Yellow) color format.
+    CMY(u8, u8, u8),
+    /// A representation of the CMYK (Cyan, Magenta, Yellow, Key) color format.
+    CMYK(u8, u8, u8, u8),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    PercentageOverflow,
+    DegreeOverflow,
+    Unimplemented,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Color::Red => write!(f, "color(Red)"),
+            Color::Green => write!(f, "color(Green)"),
+            Color::Blue => write!(f, "color(Blue)"),
+            Color::RGB(red, green, blue) => write!(f, "rgb({}, {}, {})", red, green, blue),
+            Color::RGBA(red, green, blue, alpha) => {
+                write!(f, "rgba({}, {}, {}, {})", red, green, blue, alpha)
+            }
+            Color::HSV(hue, saturation, value) => {
+                write!(f, "hsv({}, {}, {})", hue, saturation, value)
+            }
+            Color::HSL(hue, saturation, lightness) => {
+                write!(f, "hsl({}°, {}%, {}%)", hue, saturation, lightness)
+            }
+            Color::CMY(cyan, magenta, yellow) => {
+                write!(f, "cmy({}%, {}%, {}%)", cyan, magenta, yellow)
+            }
+            Color::CMYK(cyan, magenta, yellow, key) => {
+                write!(f, "cmyk({}%, {}%, {}%, {}%)", cyan, magenta, yellow, key)
+            }
+        }
+    }
+}
+
+impl Color {
+    pub fn to_rgb(&self) -> Result<Color, Error> {
+        match self {
+            Color::Red => Ok(Color::RGB(255, 0, 0)),
+            Color::Green => Ok(Color::RGB(0, 255, 0)),
+            Color::Blue => Ok(Color::RGB(0, 0, 255)),
+            Color::RGB(_, _, _) => Ok(self.clone()),
+            Color::RGBA(red, green, blue, _) => Ok(Color::RGB(*red, *green, *blue)),
+            Color::HSV(_, _, _) => Err(Error::Unimplemented),
+            Color::HSL(hue, saturation, lightness) => {
+                let c = (1. - ((2. * (*lightness as f64 / 100.)) - 1.).abs())
+                    * (*saturation as f64 / 100.);
+                let x = c * (1. - ((((*hue as f64) / 60.) % 2.) - 1.).abs());
+                let m = (*lightness as f64 / 100.) - (c / 2.);
+
+                let (r_prime, g_prime, b_prime) = match *hue {
+                    0..=59 => (c, x, 0.),
+                    60..=119 => (x, c, 0.),
+                    120..=179 => (0., c, x),
+                    180..=239 => (0., x, c),
+                    240..=299 => (x, 0., c),
+                    300..=360 => (c, 0., x),
+                    _ => {
+                        return Err(Error::DegreeOverflow)
+                    }
+                };
+
+                let apply = |v: f64| ((v + m) * 255.).round() as u8;
+                let red = apply(r_prime);
+                let green = apply(g_prime);
+                let blue = apply(b_prime);
+
+                Ok(Color::RGB(red, green, blue))
+            }
+            Color::CMY(_, _, _) => Err(Error::Unimplemented),
+            Color::CMYK(cyan, magenta, yellow, key) => {
+                let apply =
+                    |v| (255. * (1f64 - v as f64 / 100.) * (1. - *key as f64 / 100.)).round() as u8;
+
+                let red = apply(*cyan);
+                let green = apply(*magenta);
+                let blue = apply(*yellow);
+
+                Ok(Color::RGB(red, green, blue))
+            }
+        }
+    }
+
+    pub fn to_cmyk(&self) -> Result<Color, Error> {
+        unimplemented!()
+    }
+
+    pub fn to_hsl(&self) -> Result<Color, Error> {
+        unimplemented!()
+    }
+
+    pub fn to_hex_string(&self) -> String {
+        match self {
+            Color::Red => format!("#ff0000"),
+            Color::Green => format!("#00ff00"),
+            Color::Blue => format!("#0000ff"),
+            Color::RGB(red, green, blue) => format!("#{:0>2x}{:0>2x}{:0>2x}", red, green, blue),
+            Color::RGBA(red, green, blue, alpha) => {
+                format!("#{:0>2x}{:0>2x}{:0>2x}{:0>2x}", red, green, blue, alpha)
+            }
+            Color::HSV(_, _, _) => {
+                self.to_rgb().unwrap().to_hex_string()
+            }
+            Color::HSL(_, _, _) => {
+                self.to_rgb().unwrap().to_hex_string()
+            }
+            Color::CMY(_, _, _) => {
+                self.to_rgb().unwrap().to_hex_string()
+            }
+            Color::CMYK(_, _, _, _) => {
+                self.to_rgb().unwrap().to_hex_string()
+            }
+        }
+    }
 }
---
+++ src/color/palette.rs
@@ -0,0 +1,147 @@
+use super::Color;
+
+pub const WHITE: Color = Color::RGB(255, 255, 255);
+pub const BLACK: Color = Color::RGB(0, 0, 0);
+
+pub const GRAY_0: Color = Color::RGB(248, 249, 250);
+pub const GRAY_1: Color = Color::RGB(241, 243, 245);
+pub const GRAY_2: Color = Color::RGB(233, 236, 239);
+pub const GRAY_3: Color = Color::RGB(222, 226, 230);
+pub const GRAY_4: Color = Color::RGB(206, 212, 218);
+pub const GRAY_5: Color = Color::RGB(173, 181, 189);
+pub const GRAY_6: Color = Color::RGB(134, 142, 150);
+pub const GRAY_7: Color = Color::RGB(73, 80, 87);
+pub const GRAY_8: Color = Color::RGB(52, 58, 64);
+pub const GRAY_9: Color = Color::RGB(33, 37, 41);
+
+pub const RED_0: Color = Color::RGB(255, 245, 245);
+pub const RED_1: Color = Color::RGB(255, 227, 227);
+pub const RED_2: Color = Color::RGB(255, 201, 201);
+pub const RED_3: Color = Color::RGB(255, 168, 168);
+pub const RED_4: Color = Color::RGB(255, 135, 135);
+pub const RED_5: Color = Color::RGB(255, 107, 107);
+pub const RED_6: Color = Color::RGB(250, 82, 82);
+pub const RED_7: Color = Color::RGB(240, 62, 62);
+pub const RED_8: Color = Color::RGB(224, 49, 49);
+pub const RED_9: Color = Color::RGB(201, 42, 42);
+
+pub const PINK_0: Color = Color::RGB(255, 240, 246);
+pub const PINK_1: Color = Color::RGB(255, 222, 235);
+pub const PINK_2: Color = Color::RGB(252, 194, 215);
+pub const PINK_3: Color = Color::RGB(250, 162, 193);
+pub const PINK_4: Color = Color::RGB(247, 131, 172);
+pub const PINK_5: Color = Color::RGB(240, 101, 149);
+pub const PINK_6: Color = Color::RGB(230, 73, 128);
+pub const PINK_7: Color = Color::RGB(214, 51, 108);
+pub const PINK_8: Color = Color::RGB(194, 37, 92);
+pub const PINK_9: Color = Color::RGB(166, 30, 77);
+
+pub const GRAPE_0: Color = Color::RGB(248, 240, 252);
+pub const GRAPE_1: Color = Color::RGB(243, 217, 250);
+pub const GRAPE_2: Color = Color::RGB(238, 190, 250);
+pub const GRAPE_3: Color = Color::RGB(229, 153, 247);
+pub const GRAPE_4: Color = Color::RGB(218, 119, 242);
+pub const GRAPE_5: Color = Color::RGB(204, 93, 232);
+pub const GRAPE_6: Color = Color::RGB(190, 75, 219);
+pub const GRAPE_7: Color = Color::RGB(174, 62, 201);
+pub const GRAPE_8: Color = Color::RGB(156, 54, 181);
+pub const GRAPE_9: Color = Color::RGB(134, 46, 156);
+
+pub const VIOLET_0: Color = Color::RGB(243, 240, 255);
+pub const VIOLET_1: Color = Color::RGB(229, 219, 255);
+pub const VIOLET_2: Color = Color::RGB(208, 191, 255);
+pub const VIOLET_3: Color = Color::RGB(177, 151, 252);
+pub const VIOLET_4: Color = Color::RGB(151, 117, 250);
+pub const VIOLET_5: Color = Color::RGB(132, 94, 247);
+pub const VIOLET_6: Color = Color::RGB(121, 80, 242);
+pub const VIOLET_7: Color = Color::RGB(112, 72, 232);
+pub const VIOLET_8: Color = Color::RGB(103, 65, 217);
+pub const VIOLET_9: Color = Color::RGB(95, 61, 196);
+
+pub const INDIGO_0: Color = Color::RGB(237, 242, 255);
+pub const INDIGO_1: Color = Color::RGB(219, 228, 255);
+pub const INDIGO_2: Color = Color::RGB(186, 200, 255);
+pub const INDIGO_3: Color = Color::RGB(145, 167, 255);
+pub const INDIGO_4: Color = Color::RGB(116, 143, 252);
+pub const INDIGO_5: Color = Color::RGB(92, 124, 250);
+pub const INDIGO_6: Color = Color::RGB(76, 110, 245);
+pub const INDIGO_7: Color = Color::RGB(66, 99, 235);
+pub const INDIGO_8: Color = Color::RGB(59, 91, 219);
+pub const INDIGO_9: Color = Color::RGB(54, 79, 199);
+
+pub const BLUE_0: Color = Color::RGB(231, 245, 255);
+pub const BLUE_1: Color = Color::RGB(208, 235, 255);
+pub const BLUE_2: Color = Color::RGB(165, 216, 255);
+pub const BLUE_3: Color = Color::RGB(116, 192, 252);
+pub const BLUE_4: Color = Color::RGB(77, 171, 247);
+pub const BLUE_5: Color = Color::RGB(51, 154, 240);
+pub const BLUE_6: Color = Color::RGB(34, 139, 230);
+pub const BLUE_7: Color = Color::RGB(28, 126, 214);
+pub const BLUE_8: Color = Color::RGB(25, 113, 194);
+pub const BLUE_9: Color = Color::RGB(24, 100, 171);
+
+pub const CYAN_0: Color = Color::RGB(227, 250, 252);
+pub const CYAN_1: Color = Color::RGB(197, 246, 250);
+pub const CYAN_2: Color = Color::RGB(153, 233, 242);
+pub const CYAN_3: Color = Color::RGB(102, 217, 232);
+pub const CYAN_4: Color = Color::RGB(59, 201, 219);
+pub const CYAN_5: Color = Color::RGB(34, 184, 207);
+pub const CYAN_6: Color = Color::RGB(21, 170, 191);
+pub const CYAN_7: Color = Color::RGB(16, 152, 173);
+pub const CYAN_8: Color = Color::RGB(12, 133, 153);
+pub const CYAN_9: Color = Color::RGB(11, 114, 133);
+
+pub const TEAL_0: Color = Color::RGB(230, 252, 245);
+pub const TEAL_1: Color = Color::RGB(195, 250, 232);
+pub const TEAL_2: Color = Color::RGB(150, 242, 215);
+pub const TEAL_3: Color = Color::RGB(99, 230, 190);
+pub const TEAL_4: Color = Color::RGB(56, 217, 169);
+pub const TEAL_5: Color = Color::RGB(32, 201, 151);
+pub const TEAL_6: Color = Color::RGB(18, 184, 134);
+pub const TEAL_7: Color = Color::RGB(12, 166, 120);
+pub const TEAL_8: Color = Color::RGB(9, 146, 104);
+pub const TEAL_9: Color = Color::RGB(8, 127, 91);
+
+pub const GREEN_0: Color = Color::RGB(235, 251, 238);
+pub const GREEN_1: Color = Color::RGB(211, 249, 216);
+pub const GREEN_2: Color = Color::RGB(178, 242, 187);
+pub const GREEN_3: Color = Color::RGB(140, 233, 154);
+pub const GREEN_4: Color = Color::RGB(105, 219, 124);
+pub const GREEN_5: Color = Color::RGB(81, 207, 102);
+pub const GREEN_6: Color = Color::RGB(64, 192, 87);
+pub const GREEN_7: Color = Color::RGB(55, 178, 77);
+pub const GREEN_8: Color = Color::RGB(47, 158, 68);
+pub const GREEN_9: Color = Color::RGB(43, 138, 62);
+
+pub const LIME_0: Color = Color::RGB(244, 252, 227);
+pub const LIME_1: Color = Color::RGB(233, 250, 200);
+pub const LIME_2: Color = Color::RGB(216, 245, 162);
+pub const LIME_3: Color = Color::RGB(192, 235, 117);
+pub const LIME_4: Color = Color::RGB(169, 227, 75);
+pub const LIME_5: Color = Color::RGB(148, 216, 45);
+pub const LIME_6: Color = Color::RGB(130, 201, 30);
+pub const LIME_7: Color = Color::RGB(116, 184, 22);
+pub const LIME_8: Color = Color::RGB(102, 168, 15);
+pub const LIME_9: Color = Color::RGB(92, 148, 13);
+
+pub const YELLOW_0: Color = Color::RGB(255, 249, 219);
+pub const YELLOW_1: Color = Color::RGB(255, 243, 191);
+pub const YELLOW_2: Color = Color::RGB(255, 236, 153);
+pub const YELLOW_3: Color = Color::RGB(255, 224, 102);
+pub const YELLOW_4: Color = Color::RGB(255, 212, 59);
+pub const YELLOW_5: Color = Color::RGB(252, 196, 25);
+pub const YELLOW_6: Color = Color::RGB(250, 176, 5);
+pub const YELLOW_7: Color = Color::RGB(245, 159, 0);
+pub const YELLOW_8: Color = Color::RGB(240, 140, 0);
+pub const YELLOW_9: Color = Color::RGB(230, 119, 0);
+
+pub const ORANGE_0: Color = Color::RGB(255, 244, 230);
+pub const ORANGE_1: Color = Color::RGB(255, 232, 204);
+pub const ORANGE_2: Color = Color::RGB(255, 216, 168);
+pub const ORANGE_3: Color = Color::RGB(255, 192, 120);
+pub const ORANGE_4: Color = Color::RGB(255, 169, 77);
+pub const ORANGE_5: Color = Color::RGB(255, 146, 43);
+pub const ORANGE_6: Color = Color::RGB(253, 126, 20);
+pub const ORANGE_7: Color = Color::RGB(247, 103, 7);
+pub const ORANGE_8: Color = Color::RGB(232, 89, 12);
+pub const ORANGE_9: Color = Color::RGB(217, 72, 15);
