76eefe461e3c676e489091eb16c17f47b0e4b056
pr0n1x <pr0n1x@yandex.ru>
2021-03-12 01:01:08 +0700

    Refactored color::css module. Added implementations From<&str> and From<u32> for Color. More ideomatic code

--- src/color/css.rs
+++ src/color/css.rs
@@ -1,6 +1,189 @@
 use super::Color;
 use std::collections::HashMap;
 
+lazy_static! {
+    static ref COLORS: HashMap<&'static str, Color> = {
+        let colors: Vec<(&'static str, u32)> = vec![
+            // css 1
+            ("black",   0x000000),
+            ("silver",  0xc0c0c0),
+            ("gray",    0x808080),
+            ("white",   0xffffff),
+            ("maroon",  0x800000),
+            ("red",     0xff0000),
+            ("purple",  0x800080),
+            ("fuchsia", 0xff00ff),
+            ("green",   0x008000),
+            ("lime",    0x00ff00),
+            ("olive",   0x808000),
+            ("yellow",  0xffff00),
+            ("navy",    0x000080),
+            ("blue",    0x0000ff),
+            ("teal",    0x008080),
+            ("aqua",    0x00ffff),
+
+            // css2 rev 1
+            ("orange",          0xffa500),
+            ("aliceblue",       0xf0f8ff),
+            ("antiquewhite",    0xfaebd7),
+            ("aquamarine",      0x7fffd4),
+            ("azure",           0xf0ffff),
+            ("beige",           0xf5f5dc),
+            ("bisque",          0xffe4c4),
+            ("blanchedalmond",  0xffebcd),
+            ("blueviolet",      0x8a2be2),
+            ("brown",           0xa52a2a),
+            ("burlywood",       0xdeb887),
+            ("cadetblue",       0x5f9ea0),
+            ("chartreuse",      0x7fff00),
+            ("chocolate",       0xd2691e),
+            ("coral",           0xff7f50),
+            ("cornflowerblue",  0x6495ed),
+            ("cornsilk",        0xfff8dc),
+            ("crimson",         0xdc143c),
+            ("cyan",            0x00ffff),
+            ("darkblue",        0x00008b),
+            ("darkcyan",        0x008b8b),
+            ("darkgoldenrod",   0xb8860b),
+            ("darkgray",        0xa9a9a9),
+            ("darkgreen",       0x006400),
+            ("darkgrey",        0xa9a9a9),
+            ("darkkhaki",       0xbdb76b),
+            ("darkmagenta",     0x8b008b),
+            ("darkolivegreen",  0x556b2f),
+            ("darkorange",      0xff8c00),
+            ("darkorchid",      0x9932cc),
+            ("darkred",         0x8b0000),
+            ("darksalmon",      0xe9967a),
+            ("darkseagreen",    0x8fbc8f),
+            ("darkslateblue",   0x483d8b),
+            ("darkslategray",   0x2f4f4f),
+            ("darkslategrey",   0x2f4f4f),
+            ("darkturquoise",   0x00ced1),
+            ("darkviolet",      0x9400d3),
+            ("deeppink",        0xff1493),
+            ("deepskyblue",     0x00bfff),
+            ("dimgray",         0x696969),
+            ("dimgrey",         0x696969),
+            ("dodgerblue",      0x1e90ff),
+            ("firebrick",       0xb22222),
+            ("floralwhite",     0xfffaf0),
+            ("forestgreen",     0x228b22),
+            ("gainsboro",       0xdcdcdc),
+            ("ghostwhite",      0xf8f8ff),
+            ("gold",            0xffd700),
+            ("goldenrod",       0xdaa520),
+            ("greenyellow",     0xadff2f),
+            ("grey",            0x808080),
+            ("honeydew",        0xf0fff0),
+            ("hotpink",         0xff69b4),
+            ("indianred",       0xcd5c5c),
+            ("indigo",          0x4b0082),
+            ("ivory",           0xfffff0),
+            ("khaki",           0xf0e68c),
+            ("lavender",        0xe6e6fa),
+            ("lavenderblush",   0xfff0f5),
+            ("lawngreen",       0x7cfc00),
+            ("lemonchiffon",    0xfffacd),
+            ("lightblue",       0xadd8e6),
+            ("lightcoral",      0xf08080),
+            ("lightcyan",       0xe0ffff),
+            ("lightgoldenrodyellow", 0xfafad2),
+
+            // css3
+            ("lightgray",           0xd3d3d3),
+            ("lightgreen",          0x90ee90),
+            ("lightgrey",           0xd3d3d3),
+            ("lightpink",           0xffb6c1),
+            ("lightsalmon",         0xffa07a),
+            ("lightseagreen",       0x20b2aa),
+            ("lightskyblue",        0x87cefa),
+            ("lightslategray",      0x778899),
+            ("lightslategrey",      0x778899),
+            ("lightsteelblue",      0xb0c4de),
+            ("lightyellow",         0xffffe0),
+            ("limegreen",           0x32cd32),
+            ("linen",               0xfaf0e6),
+            ("magenta",             0xff00ff),
+            ("mediumaquamarine",    0x66cdaa),
+            ("mediumblue",          0x0000cd),
+            ("mediumorchid",        0xba55d3),
+            ("mediumpurple",        0x9370db),
+            ("mediumseagreen",      0x3cb371),
+            ("mediumslateblue",     0x7b68ee),
+            ("mediumspringgreen",   0x00fa9a),
+            ("mediumturquoise",     0x48d1cc),
+            ("mediumvioletred",     0xc71585),
+            ("midnightblue",        0x191970),
+            ("mintcream",           0xf5fffa),
+            ("mistyrose",           0xffe4e1),
+            ("moccasin",            0xffe4b5),
+            ("navajowhite",         0xffdead),
+            ("oldlace",             0xfdf5e6),
+            ("olivedrab",           0x6b8e23),
+            ("orangered",           0xff4500),
+            ("orchid",              0xda70d6),
+            ("palegoldenrod",       0xeee8aa),
+            ("palegreen",           0x98fb98),
+            ("paleturquoise",       0xafeeee),
+            ("palevioletred",       0xdb7093),
+            ("papayawhip",          0xffefd5),
+            ("peachpuff",           0xffdab9),
+            ("peru",                0xcd853f),
+            ("pink",                0xffc0cb),
+            ("plum",                0xdda0dd),
+            ("powderblue",          0xb0e0e6),
+            ("rosybrown",           0xbc8f8f),
+            ("royalblue",           0x4169e1),
+            ("saddlebrown",         0x8b4513),
+            ("salmon",              0xfa8072),
+            ("sandybrown",          0xf4a460),
+            ("seagreen",            0x2e8b57),
+            ("seashell",            0xfff5ee),
+            ("sienna",              0xa0522d),
+            ("skyblue",             0x87ceeb),
+            ("slateblue",           0x6a5acd),
+            ("slategray",           0x708090),
+            ("slategrey",           0x708090),
+            ("snow",                0xfffafa),
+            ("springgreen",         0x00ff7f),
+            ("steelblue",           0x4682b4),
+            ("tan",                 0xd2b48c),
+            ("thistle",             0xd8bfd8),
+            ("tomato",              0xff6347),
+            ("turquoise",           0x40e0d0),
+            ("violet",              0xee82ee),
+            ("wheat",               0xf5deb3),
+            ("whitesmoke",          0xf5f5f5),
+            ("yellowgreen",         0x9acd32),
+
+            // css4
+            ("rebeccapurple",       0x663399),
+        ];
+        let mut map = HashMap::new();
+        for (color_name, color_u32) in colors.iter() {
+            map.insert(*color_name, (*color_u32).into());
+        }
+        map
+    };
+}
+
+impl From<u32> for Color {
+    fn from(color_u32: u32) -> Self {
+        Self::from_rgb_u32(color_u32)
+    }
+}
+
+impl From<&str> for Color {
+    fn from(color_str: &str) -> Self {
+        if !color_str.starts_with('#') {
+            Self::from_hex_str(color_str)
+        } else {
+            Self::from_css_name(color_str)
+        }
+    }
+}
+
 impl Color {
     pub fn from_css_name(name: &str) -> Self {
         COLORS.get(name).expect("Unknown css name of color").clone()
@@ -36,12 +219,12 @@ impl Color {
         Self::RGB(red, green, blue)
     }
 
-    fn from_short_rgba_u16(c: u16) -> Self {
+    pub fn from_short_rgba_u16(c: u16) -> Self {
         let (red, green, blue, alpha) = Self::from_short_rgba_u16_to_tuple(c);
         Self::RGBA(red, green, blue, alpha)
     }
 
-    fn from_rgba_u32(c: u32) -> Self {
+    pub fn from_rgba_u32(c: u32) -> Self {
         let (red, green, blue, alpha) = Self::from_rgba_u32_to_tuple(c);
         Self::RGBA(red, green, blue, alpha)
     }
@@ -85,170 +268,6 @@ impl Color {
             (c & 0x000000ff) as u8
         )
     }
-}
-
-lazy_static! {
-    static ref COLORS: HashMap<&'static str, Color> = {
-        let mut c = HashMap::new();
-
-        // css 1
-        c.insert("black",   Color::from_rgb_u32(0x000000));
-        c.insert("silver",  Color::from_rgb_u32(0xc0c0c0));
-        c.insert("gray",    Color::from_rgb_u32(0x808080));
-        c.insert("white",   Color::from_rgb_u32(0xffffff));
-        c.insert("maroon",  Color::from_rgb_u32(0x800000));
-        c.insert("red",     Color::from_rgb_u32(0xff0000));
-        c.insert("purple",  Color::from_rgb_u32(0x800080));
-        c.insert("fuchsia", Color::from_rgb_u32(0xff00ff));
-        c.insert("green",   Color::from_rgb_u32(0x008000));
-        c.insert("lime",    Color::from_rgb_u32(0x00ff00));
-        c.insert("olive",   Color::from_rgb_u32(0x808000));
-        c.insert("yellow",  Color::from_rgb_u32(0xffff00));
-        c.insert("navy",    Color::from_rgb_u32(0x000080));
-        c.insert("blue",    Color::from_rgb_u32(0x0000ff));
-        c.insert("teal",    Color::from_rgb_u32(0x008080));
-        c.insert("aqua",    Color::from_rgb_u32(0x00ffff));
-
-        // css2 rev 1
-        c.insert("orange",          Color::from_rgb_u32(0xffa500));
-        c.insert("aliceblue",       Color::from_rgb_u32(0xf0f8ff));
-        c.insert("antiquewhite",    Color::from_rgb_u32(0xfaebd7));
-        c.insert("aquamarine",      Color::from_rgb_u32(0x7fffd4));
-        c.insert("azure",           Color::from_rgb_u32(0xf0ffff));
-        c.insert("beige",           Color::from_rgb_u32(0xf5f5dc));
-        c.insert("bisque",          Color::from_rgb_u32(0xffe4c4));
-        c.insert("blanchedalmond",  Color::from_rgb_u32(0xffebcd));
-        c.insert("blueviolet",      Color::from_rgb_u32(0x8a2be2));
-        c.insert("brown",           Color::from_rgb_u32(0xa52a2a));
-        c.insert("burlywood",       Color::from_rgb_u32(0xdeb887));
-        c.insert("cadetblue",       Color::from_rgb_u32(0x5f9ea0));
-        c.insert("chartreuse",      Color::from_rgb_u32(0x7fff00));
-        c.insert("chocolate",       Color::from_rgb_u32(0xd2691e));
-        c.insert("coral",           Color::from_rgb_u32(0xff7f50));
-        c.insert("cornflowerblue",  Color::from_rgb_u32(0x6495ed));
-        c.insert("cornsilk",        Color::from_rgb_u32(0xfff8dc));
-        c.insert("crimson",         Color::from_rgb_u32(0xdc143c));
-        c.insert("cyan",            Color::from_rgb_u32(0x00ffff));
-        c.insert("darkblue",        Color::from_rgb_u32(0x00008b));
-        c.insert("darkcyan",        Color::from_rgb_u32(0x008b8b));
-        c.insert("darkgoldenrod",   Color::from_rgb_u32(0xb8860b));
-        c.insert("darkgray",        Color::from_rgb_u32(0xa9a9a9));
-        c.insert("darkgreen",       Color::from_rgb_u32(0x006400));
-        c.insert("darkgrey",        Color::from_rgb_u32(0xa9a9a9));
-        c.insert("darkkhaki",       Color::from_rgb_u32(0xbdb76b));
-        c.insert("darkmagenta",     Color::from_rgb_u32(0x8b008b));
-        c.insert("darkolivegreen",  Color::from_rgb_u32(0x556b2f));
-        c.insert("darkorange",      Color::from_rgb_u32(0xff8c00));
-        c.insert("darkorchid",      Color::from_rgb_u32(0x9932cc));
-        c.insert("darkred",         Color::from_rgb_u32(0x8b0000));
-        c.insert("darksalmon",      Color::from_rgb_u32(0xe9967a));
-        c.insert("darkseagreen",    Color::from_rgb_u32(0x8fbc8f));
-        c.insert("darkslateblue",   Color::from_rgb_u32(0x483d8b));
-        c.insert("darkslategray",   Color::from_rgb_u32(0x2f4f4f));
-        c.insert("darkslategrey",   Color::from_rgb_u32(0x2f4f4f));
-        c.insert("darkturquoise",   Color::from_rgb_u32(0x00ced1));
-        c.insert("darkviolet",      Color::from_rgb_u32(0x9400d3));
-        c.insert("deeppink",        Color::from_rgb_u32(0xff1493));
-        c.insert("deepskyblue",     Color::from_rgb_u32(0x00bfff));
-        c.insert("dimgray",         Color::from_rgb_u32(0x696969));
-        c.insert("dimgrey",         Color::from_rgb_u32(0x696969));
-        c.insert("dodgerblue",      Color::from_rgb_u32(0x1e90ff));
-        c.insert("firebrick",       Color::from_rgb_u32(0xb22222));
-        c.insert("floralwhite",     Color::from_rgb_u32(0xfffaf0));
-        c.insert("forestgreen",     Color::from_rgb_u32(0x228b22));
-        c.insert("gainsboro",       Color::from_rgb_u32(0xdcdcdc));
-        c.insert("ghostwhite",      Color::from_rgb_u32(0xf8f8ff));
-        c.insert("gold",            Color::from_rgb_u32(0xffd700));
-        c.insert("goldenrod",       Color::from_rgb_u32(0xdaa520));
-        c.insert("greenyellow",     Color::from_rgb_u32(0xadff2f));
-        c.insert("grey",            Color::from_rgb_u32(0x808080));
-        c.insert("honeydew",        Color::from_rgb_u32(0xf0fff0));
-        c.insert("hotpink",         Color::from_rgb_u32(0xff69b4));
-        c.insert("indianred",       Color::from_rgb_u32(0xcd5c5c));
-        c.insert("indigo",          Color::from_rgb_u32(0x4b0082));
-        c.insert("ivory",           Color::from_rgb_u32(0xfffff0));
-        c.insert("khaki",           Color::from_rgb_u32(0xf0e68c));
-        c.insert("lavender",        Color::from_rgb_u32(0xe6e6fa));
-        c.insert("lavenderblush",   Color::from_rgb_u32(0xfff0f5));
-        c.insert("lawngreen",       Color::from_rgb_u32(0x7cfc00));
-        c.insert("lemonchiffon",    Color::from_rgb_u32(0xfffacd));
-        c.insert("lightblue",       Color::from_rgb_u32(0xadd8e6));
-        c.insert("lightcoral",      Color::from_rgb_u32(0xf08080));
-        c.insert("lightcyan",       Color::from_rgb_u32(0xe0ffff));
-        c.insert("lightgoldenrodyellow", Color::from_rgb_u32(0xfafad2));
-
-        //css3
-        c.insert("lightgray",           Color::from_rgb_u32(0xd3d3d3));
-        c.insert("lightgreen",          Color::from_rgb_u32(0x90ee90));
-        c.insert("lightgrey",           Color::from_rgb_u32(0xd3d3d3));
-        c.insert("lightpink",           Color::from_rgb_u32(0xffb6c1));
-        c.insert("lightsalmon",         Color::from_rgb_u32(0xffa07a));
-        c.insert("lightseagreen",       Color::from_rgb_u32(0x20b2aa));
-        c.insert("lightskyblue",        Color::from_rgb_u32(0x87cefa));
-        c.insert("lightslategray",      Color::from_rgb_u32(0x778899));
-        c.insert("lightslategrey",      Color::from_rgb_u32(0x778899));
-        c.insert("lightsteelblue",      Color::from_rgb_u32(0xb0c4de));
-        c.insert("lightyellow",         Color::from_rgb_u32(0xffffe0));
-        c.insert("limegreen",           Color::from_rgb_u32(0x32cd32));
-        c.insert("linen",               Color::from_rgb_u32(0xfaf0e6));
-        c.insert("magenta",             Color::from_rgb_u32(0xff00ff));
-        c.insert("mediumaquamarine",    Color::from_rgb_u32(0x66cdaa));
-        c.insert("mediumblue",          Color::from_rgb_u32(0x0000cd));
-        c.insert("mediumorchid",        Color::from_rgb_u32(0xba55d3));
-        c.insert("mediumpurple",        Color::from_rgb_u32(0x9370db));
-        c.insert("mediumseagreen",      Color::from_rgb_u32(0x3cb371));
-        c.insert("mediumslateblue",     Color::from_rgb_u32(0x7b68ee));
-        c.insert("mediumspringgreen",   Color::from_rgb_u32(0x00fa9a));
-        c.insert("mediumturquoise",     Color::from_rgb_u32(0x48d1cc));
-        c.insert("mediumvioletred",     Color::from_rgb_u32(0xc71585));
-        c.insert("midnightblue",        Color::from_rgb_u32(0x191970));
-        c.insert("mintcream",           Color::from_rgb_u32(0xf5fffa));
-        c.insert("mistyrose",           Color::from_rgb_u32(0xffe4e1));
-        c.insert("moccasin",            Color::from_rgb_u32(0xffe4b5));
-        c.insert("navajowhite",         Color::from_rgb_u32(0xffdead));
-        c.insert("oldlace",             Color::from_rgb_u32(0xfdf5e6));
-        c.insert("olivedrab",           Color::from_rgb_u32(0x6b8e23));
-        c.insert("orangered",           Color::from_rgb_u32(0xff4500));
-        c.insert("orchid",              Color::from_rgb_u32(0xda70d6));
-        c.insert("palegoldenrod",       Color::from_rgb_u32(0xeee8aa));
-        c.insert("palegreen",           Color::from_rgb_u32(0x98fb98));
-        c.insert("paleturquoise",       Color::from_rgb_u32(0xafeeee));
-        c.insert("palevioletred",       Color::from_rgb_u32(0xdb7093));
-        c.insert("papayawhip",          Color::from_rgb_u32(0xffefd5));
-        c.insert("peachpuff",           Color::from_rgb_u32(0xffdab9));
-        c.insert("peru",                Color::from_rgb_u32(0xcd853f));
-        c.insert("pink",                Color::from_rgb_u32(0xffc0cb));
-        c.insert("plum",                Color::from_rgb_u32(0xdda0dd));
-        c.insert("powderblue",          Color::from_rgb_u32(0xb0e0e6));
-        c.insert("rosybrown",           Color::from_rgb_u32(0xbc8f8f));
-        c.insert("royalblue",           Color::from_rgb_u32(0x4169e1));
-        c.insert("saddlebrown",         Color::from_rgb_u32(0x8b4513));
-        c.insert("salmon",              Color::from_rgb_u32(0xfa8072));
-        c.insert("sandybrown",          Color::from_rgb_u32(0xf4a460));
-        c.insert("seagreen",            Color::from_rgb_u32(0x2e8b57));
-        c.insert("seashell",            Color::from_rgb_u32(0xfff5ee));
-        c.insert("sienna",              Color::from_rgb_u32(0xa0522d));
-        c.insert("skyblue",             Color::from_rgb_u32(0x87ceeb));
-        c.insert("slateblue",           Color::from_rgb_u32(0x6a5acd));
-        c.insert("slategray",           Color::from_rgb_u32(0x708090));
-        c.insert("slategrey",           Color::from_rgb_u32(0x708090));
-        c.insert("snow",                Color::from_rgb_u32(0xfffafa));
-        c.insert("springgreen",         Color::from_rgb_u32(0x00ff7f));
-        c.insert("steelblue",           Color::from_rgb_u32(0x4682b4));
-        c.insert("tan",                 Color::from_rgb_u32(0xd2b48c));
-        c.insert("thistle",             Color::from_rgb_u32(0xd8bfd8));
-        c.insert("tomato",              Color::from_rgb_u32(0xff6347));
-        c.insert("turquoise",           Color::from_rgb_u32(0x40e0d0));
-        c.insert("violet",              Color::from_rgb_u32(0xee82ee));
-        c.insert("wheat",               Color::from_rgb_u32(0xf5deb3));
-        c.insert("whitesmoke",          Color::from_rgb_u32(0xf5f5f5));
-        c.insert("yellowgreen",         Color::from_rgb_u32(0x9acd32));
-
-        //css4
-        c.insert("rebeccapurple",       Color::from_rgb_u32(0x663399));
-
-        c
-    };
 }
 
 #[cfg(test)]
