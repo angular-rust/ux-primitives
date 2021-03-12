749dd30c693d290d8d9ad7e2b7f2a9a682faaa58
pr0n1x <pr0n1x@yandex.ru>
2021-03-12 16:39:56 +0700

    Refactored functions of color module

--- src/color/utils.rs
+++ src/color/utils.rs
@@ -1,7 +1,7 @@
 use super::Color;
 use super::palette::*;
 
-pub fn get_palette_vec() -> Vec<Color> {
+pub fn palette_vec() -> Vec<Color> {
     vec!(
         BLACK, WHITE,
         GRAY_0, GRAY_1, GRAY_2, GRAY_3, GRAY_4, GRAY_5, GRAY_6, GRAY_7, GRAY_8, GRAY_9,
@@ -18,4 +18,44 @@ pub fn get_palette_vec() -> Vec<Color> {
         YELLOW_0, YELLOW_1, YELLOW_2, YELLOW_3, YELLOW_4, YELLOW_5, YELLOW_6, YELLOW_7, YELLOW_8, YELLOW_9,
         ORANGE_0, ORANGE_1, ORANGE_2, ORANGE_3, ORANGE_4, ORANGE_5, ORANGE_6, ORANGE_7, ORANGE_8, ORANGE_9,
     )
+}
+
+pub fn color_from_short_rgb_u16(c: u16) -> Color {
+    let (red, green, blue, _) = (
+        ((c >> 8) + ((c >> 8) << 4)) as u8,
+        ((c & 0x0f0) + ((c & 0x0f0) >> 4)) as u8,
+        ((c & 0x00f) + ((c & 0x00f) << 4)) as u8,
+        0xff
+    );
+    Color::RGB(red, green, blue)
+}
+
+pub fn color_from_rgb_u32(c: u32) -> Color {
+    let (red, green, blue, _) = (
+        (c >> 16) as u8,
+        ((c & 0x00ff00) >> 8) as u8,
+        (c & 0x0000ff) as u8,
+        0xff
+    );
+    Color::RGB(red, green, blue)
+}
+
+pub fn color_from_short_rgba_u16(c: u16) -> Color {
+    let (red, green, blue, alpha) = (
+        ((c >> 12) + ((c >> 12) << 4)) as u8,
+        (((c & 0x0f00) >> 4) + ((c & 0x0f00) >> 8)) as u8,
+        ((c & 0x00f0) + ((c & 0x00f0) >> 4)) as u8,
+        ((c & 0x000f) + ((c & 0x000f) << 4)) as u8
+    );
+    Color::RGBA(red, green, blue, alpha)
+}
+
+pub fn color_from_rgba_u32(c: u32) -> Color {
+    let (red, green, blue, alpha) = (
+        (c >> 24) as u8,
+        ((c & 0x00ff0000) >> 16) as u8,
+        ((c & 0x0000ff00) >> 8) as u8,
+        (c & 0x000000ff) as u8
+    );
+    Color::RGBA(red, green, blue, alpha)
 }
