f9a2aa44c3754ca0028e4592a1d12945a5058f27
Victor Dudochkin <dudochkin.victor@gmail.com>
2021-04-09 12:50:50 +0700

    Go to unclock gradient and pattern

--- src/canvas/style.rs
+++ src/canvas/style.rs
@@ -1,19 +1,45 @@
 #![allow(dead_code)]
 use crate::color::Color;
 
-pub trait CanvasGradientInterface {
+pub struct RadialGradient {
+    x0: f64,
+    y0: f64,
+    r0: f64,
+    x1: f64,
+    y1: f64,
+    r1: f64,
+}
+
+pub struct LinearGradient {
+    x0: f64,
+    y0: f64,
+    x1: f64,
+    y1: f64,
+}
+
+pub struct ColorStop {
+    pub offset: f64,
+    pub color: Color,
+}
+
+pub struct Pattern {
 
 }
 
-pub trait CanvasPatternInterface {
+pub struct Gradient {
 
 }
 
-pub struct CanvasStyle<G, P> where
-    G: CanvasGradientInterface,
-    P: CanvasPatternInterface
-{
-    color: Color,
-    gradient: G,
-    pattern: P
+impl Gradient {
+    pub fn add_color_stop(&self, _stop: ColorStop) {
+
+    }
+
+    pub fn get_color_count(&self) -> usize {
+        unimplemented!()
+    }
+
+    pub fn get_color_stop(&self, _index: usize) -> Option<ColorStop> {
+        unimplemented!()
+    }
 }
